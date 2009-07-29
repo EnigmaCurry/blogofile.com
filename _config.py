bc2961bfeaf920bc9571100e1680359e9c0cbaf6
Ryan McGuire <ryan@Sovreign.(none)>
2009-07-28 23:46:29 -0400

    migrate to new configuration format

---
+++ _config.py
@@ -0,0 +1,17 @@
+blogofile_config = {
+    "blog_name" : "Blogofile",
+    "blog_url"  : "http://www.blogofile.com",
+    "blog_description" : "A static blog engine/compiler",
+    "timezone"  : "US/Eastern"
+    }
+
+disqus_config = {
+    "enabled" : True,
+    "disqus_name": "blogofile"
+    }
+
+syntax_highlighting_config = {
+    "enabled" : True,
+    "style" : "murphy"
+    }
+
