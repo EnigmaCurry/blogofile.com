17d11cc517d407d56ad6fb9c1362c0808b32d0e1
Ryan McGuire <ryan@enigmacurry.com>
2009-08-26 22:59:08 -0400

    fix ignore patterns to be compatible with Win32

--- _config.py
+++ _config.py
@@ -127,15 +127,15 @@ site_css_dir = "/css"
 # These can be strings or compiled patterns.
 # Strings are assumed to be case insensitive.
 ignore_patterns = [
-    r".*[\/]_.*",   #All files that start with an underscore
-    r".*[\/]#.*",   #Emacs temporary files
-    r".*[\/]\.#.*", #Emacs temporary files
+    r".*([\/]|[\\])_.*",   #All files that start with an underscore
+    r".*([\/]|[\\])#.*",   #Emacs temporary files
+    r".*([\/]|[\\])\.#.*", #Emacs temporary files
     r".*~$]",       #Emacs temporary files
-    r".*[\/]\.git", #Git VCS dir
-    r".*[\/]\.hg",  #Mercurial VCS dir
-    r".*[\/]\.bzr", #Bazaar VCS dir
-    r".*[\/]\.svn", #Subversion VCS dir
-    r".*[\/]CVS"    #CVS dir
+    r".*([\/]|[\\])\.git", #Git VCS dir
+    r".*([\/]|[\\])\.hg",  #Mercurial VCS dir
+    r".*([\/]|[\\])\.bzr", #Bazaar VCS dir
+    r".*([\/]|[\\])\.svn", #Subversion VCS dir
+    r".*([\/]|[\\])CVS"    #CVS dir
     ]
 
 ### Pre/Post build hooks:
