e6d8d55b2b9d0d6e8d96e36113568b900be76550
Ryan McGuire <ryan@enigmacurry.com>
2009-11-30 20:21:33 -0500

    refactor controllers to use the new writer.materialize_template method

--- _controllers/permapage.py
+++ _controllers/permapage.py
@@ -6,14 +6,11 @@ def run():
 
 def write_permapages():
     "Write blog posts to their permalink locations"
-    perma_template = bf.writer.template_lookup.get_template("permapage.mako")
-    perma_template.output_encoding = "utf-8"
     for post in bf.posts:
         if post.permalink:
-            path_parts = [bf.writer.output_dir]
-            path_parts.extend(urlparse.urlparse(
-                    post.permalink)[2].lstrip("/").split("/"))
-            path = bf.util.path_join(*path_parts)
+            path = urlparse.urlparse(
+                post.permalink)[2].lstrip("/").split("/")
+            path = bf.util.path_join(path)
             bf.logger.info("Writing permapage for post: "+path)
         else:
             #Permalinks MUST be specified. No permalink, no page.
@@ -23,10 +20,7 @@ def write_permapages():
             bf.util.mkdir(path)
         except OSError:
             pass
-        html = bf.writer.template_render(
-            perma_template,
-            { "post": post,
-              "posts": bf.posts })
-        f = open(bf.util.path_join(path,"index.html"), "w")
-        f.write(html)
-        f.close()
+        bf.writer.materialize_template(
+            "permapage.mako", bf.util.path_join(path,"index.html"), 
+            { "post": post, "posts": bf.posts })
+        
