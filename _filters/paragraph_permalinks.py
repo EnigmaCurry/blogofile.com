5c84b529d09ba202fedbb68bf35a6fc550bbf17e
Ryan McGuire <ryan@enigmacurry.com>
2011-01-01 13:19:51 -0500

    Use PyQuery paragraph permalinks, ET version sucks :(

--- _filters/paragraph_permalinks.py
+++ _filters/paragraph_permalinks.py
@@ -2,14 +2,32 @@
 #This allows you to reference individual paragraphs by permalink:
 # http://yourblog.com/blog/this-is-a-post#p5
 
-import xml.etree.ElementTree as et
+# PyQuery version. Works great, is forgiving of bad HTML, but requries libxml
+# which is not likely installed on the user's machine:
+
+from pyquery import PyQuery as pq
 
 def run(content):
-    #ElementTree requires a containing element, so just use <xml>
-    d = et.fromstring("<xml>%s</xml>"%content)
+    d = pq("<xml>%s</xml>"%content)
     p_num = 1
-    for p in d.getiterator("p"):
-        if p.get("id") is None:
+    for p in d("p"):
+        if not p.attrib.has_key("id"):
             p.set('id',"p"+str(p_num))
         p_num += 1
-    return et.tostring(d)
+    return d.html()
+
+# ElementTree version. Is not very forgiving of bad HTML.
+# Tidy could help but that would be another install, which if we
+# were going to do that, just use the PyQuery version above.
+
+# import xml.etree.ElementTree as et
+
+# def run(content):
+#     #ElementTree requires a containing element, so just use <xml>
+#     d = et.fromstring("<xml>%s</xml>"%content)
+#     p_num = 1
+#     for p in d.getiterator("p"):
+#         if p.get("id") is None:
+#             p.set('id',"p"+str(p_num))
+#         p_num += 1
+#     return et.tostring(d)
